3b608842d8f0370080c42d41765af17a628b3089
sabri-zaki <37634605+sabri-zaki@users.noreply.github.com>
2019-11-10 03:03:35 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,46 @@
+![1](https://user-images.githubusercontent.com/38766899/63697912-77754f80-c826-11e9-922a-a01676675b84.png)
+
+# Most-powerful proxy feeder
+This is a simple tool that will help you to find active (anonymous,elite,Transparent ect) proxies that will help you in different stuffs.
+
+## ⭕️ Features
+
+1. easy to use
+2. auto refresh in every 2 minutes
+3. it gives you all active proxies with full information
+
+just lets make life easier, take a beer and relax
+
+
+## ⭕️ Getting Started
+git clone https://github.com/mRanonyMousTZ/proxy-feeder.git
+
+cd proxy-feeder
+
+chmod +x *
+
+./setup.sh
+
+wait for installation to finish
+
+## ⭕️ Fire up a tool
+
+python proxy-feeder.py
+
+ENJOY.
+
+![proxy-feeder](https://user-images.githubusercontent.com/38766899/63697272-40eb0500-c825-11e9-911d-8e6880eaa348.png)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+
+
+BOLD="\033[01;01m"     # Highlight
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   
+RESET="\033[00m"
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+transparent="\e[0m"
+echo ""
+
+ 
+
+
+
+
+echo "================ LETS START INSTALLING SOME ESSENTIAL STUFFS ===================="
+
+echo "==========================  PROXY-FINDER SETUP ========================"
+ echo -e " $RESET****************  SABRI ZAKI*****************"                           
+
+
+sleep 2
+
+
+
+
+echo -e " $purple >>Do you want to Continue: (Y) \c"
+
+read Y
+
+if [ "$Y" ];
+then
+	apt-get update
+    apt-get upgrade
+	apt-get install git
+	apt-get install python2
+	pip2 install requests
+	pip2 install bs4
+	
+	
+sleep 6
+	
+echo -e "$RESET YOUR READY TO GO, FIRE UP A TOOL "
+
+else
+
+exit 0
+
+fi
+        
